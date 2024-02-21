959d51deeaa9e33cf062c460830f23159e2f38b2
Dennis Ma <dennisma7@gmail.com>
2024-02-21 15:17:52 -0800

    playlist modal

--- react-vite/src/components/Playlist/Playlist.jsx
+++ react-vite/src/components/Playlist/Playlist.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import OpenModalButton from "../OpenModalButton";
 import EditPlaylist from "../EditPlaylist/EditPlaylist";
@@ -6,14 +7,21 @@ import { useNavigate } from "react-router-dom";
 
 function Playlist({ playlist }) {
     const sessionUser = useSelector(state => state.session.user);
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
 
     const deletePlaylist = async e => {
-        e.preventDefault()
-        dispatch(deletePlaylistThunk(playlist.id))
-        navigate('/')
-    }
+        e.preventDefault();
+        const result = await dispatch(deletePlaylistThunk(playlist.id));
+        if (result.success) {
+            closeModal(); 
+            navigate('/');
+        }
+    };
 
     return (
         <div>
@@ -23,8 +31,15 @@ function Playlist({ playlist }) {
                     <h4>{playlist.description}</h4>
                     <h3>{sessionUser.username}</h3>
                     <OpenModalButton
-                        buttonText='...'
-                        modalComponent={<EditPlaylist playlist={playlist}/>}
+                        buttonText="..."
+                        modalComponent={
+                            <EditPlaylist 
+                                playlist={playlist} 
+                                isOpen={isModalOpen} 
+                                onRequestClose={closeModal} 
+                            />
+                        }
+                        onButtonClick={openModal} // Assuming OpenModalButton accepts an onButtonClick prop to open the modal
                     />
                     <button onClick={deletePlaylist}>Delete Playlist</button>
                 </>
