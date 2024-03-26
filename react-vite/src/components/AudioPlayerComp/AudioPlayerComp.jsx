8831f038a0c41fd306c60f7e8991c8050fdb1d51
An Ngo <anng0149@gmail.com>
2024-03-26 09:09:24 -0700

    audio player

--- react-vite/src/components/AudioPlayerComp/AudioPlayerComp.jsx
+++ react-vite/src/components/AudioPlayerComp/AudioPlayerComp.jsx
@@ -1,13 +1,24 @@
+
 import AudioPlayer from 'react-h5-audio-player'
 import 'react-h5-audio-player/lib/styles.css';
 
 
 function AudioPlayerComp({song}){
+    console.log('this is the song',song)
+    // const setHeader = ()=>{
+    //     if(Object.values(song).length){
+    //         console.log('entered')
+    //         return `${song.title} - ${song.artist?.first_name} ${song.artist?.last_name}`
+    //     }else{
+    //         return ""
+    //     }
+    // }
+
     return(
         <div>
             {/* <h3>{song && song.title}</h3> */}
             <AudioPlayer
-                header={song? `${song.title}`:""}
+                header={Object.values(song).length? `${song.title} - ${song.artist?.first_name} ${song.artist?.last_name}`:'' }
                 src={song.song_url}
                 onPlay={e => console.log(e)}
             />
